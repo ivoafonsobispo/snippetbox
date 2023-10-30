de95458a7fcd9d3e06a51f92aadb56d24d463850
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-10-30 08:16:54 +0000

    Chapter 2.3

--- main.go
+++ main.go
@@ -6,12 +6,25 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	addr := ":4000"
 	log.Println("starting server on ", addr)
