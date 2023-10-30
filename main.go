ac7a5a93420da532cd7905e29060039ac87a2e13
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-10-30 08:08:37 +0000

    Chapter 2.2

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	addr := ":4000"
+	log.Println("starting server on ", addr)
+
+	err := http.ListenAndServe(addr, mux)
+	log.Fatal(err)
 }
