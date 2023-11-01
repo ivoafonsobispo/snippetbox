4571745005764087c150d8c1b423e136bf5c2439
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-11-01 10:48:46 +0000

    Chapter 13 - File embedding

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/ivoafonsobispo/snippetbox/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -15,8 +16,8 @@ func (app *Application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
